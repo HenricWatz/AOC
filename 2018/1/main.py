bf88aa40045d4b27b9298a224dc535df0d215ca6
Henric Watz <henric.watz@live.com>
2018-12-01 11:08:13 +0100

    part two of 1 complet

--- 2018/1/main.py
+++ 2018/1/main.py
@@ -15,13 +15,39 @@ def readInput( file, type ):
 
 
 if __name__ == "__main__":
+    
+    # Read input
     num_list = readInput("input_one.txt", "scalar_list")
 
+    # Part one, find end frequency
     freq = 0
     for change in num_list:
         freq += change
 
-    print(freq)
+    print("End frequency (part 1): " + str(freq))
+
+    # Part two, find first duplicate frequency
+    freq_list = []
+    freq = 0
+    idx = 0
+    while not freq in freq_list:
+
+        # Add and calc new frequency
+        freq_list.append(freq)
+        freq += num_list[idx]
+
+        # Print length for debugging
+        print(len(freq_list))
+            
+        # Circular list 
+        idx += 1
+        if idx >= len(num_list):
+            idx = 0
+
+    print("Fist duplicate found (part 2): " + str(freq))
+
+    
+    
         
     
 
